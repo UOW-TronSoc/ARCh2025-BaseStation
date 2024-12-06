f0fbe76af991fc3d102b497025aab61942ae83ac
Soham Verma <sohamverma26@gmail.com>
2024-12-06 14:05:35 +1100

    initial react with django setup

---
+++ basestationproject/backendapi/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hello_world(request):
+    return Response({'message': 'Hello, world!'})
+
+
+# backend functional code here or where you will post to
