a42ed80fcbc4fab8746c22c51f426c4816b2906c
Soham Verma <sohamverma26@gmail.com>
2024-12-17 14:26:47 +1100

    urls naming change

--- basestationproject/backendapi/urls.py
+++ basestationproject/backendapi/urls.py
@@ -4,20 +4,20 @@ from views import *
 
 urlpatterns = [
     # API endpoint to store mock device data in Redis
-    path("api/store-devices", views.store_devices, name="store_devices"),
+    path("api/store-devices", store_devices, name="store_devices"),
 
     # API endpoint to fetch connected devices data from Redis
-    path("api/devices", views.get_devices, name="get_devices"),
+    path("api/devices", get_devices, name="get_devices"),
 
     # API endpoint to store robo spec (inventory) data in Redis
-    path("api/store-specs", views.get_robo_specs, name="store_specs"),
+    path("api/store-specs", get_robo_specs, name="store_specs"),
 
     # API endpoint to fetch robo spec (inventory) data from Redis
-    path("api/specs", views.get_specs, name="get_specs"),
+    path("api/specs", get_specs, name="get_specs"),
 
     # Optional: Endpoints for connecting and disconnecting the rover
-    path("api/connect", views.connect_rover, name="connect_rover"),
-    path("api/disconnect", views.disconnect_rover, name="disconnect_rover"),
+    path("api/connect", connect_rover, name="connect_rover"),
+    path("api/disconnect", disconnect_rover, name="disconnect_rover"),
 
     path('api/command/', RoverCommand.as_view(), name='rover-command'),
     path('api/arm-overview/', ArmOverview.as_view(), name='rover-arm-overview'),
