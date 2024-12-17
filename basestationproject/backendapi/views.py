de6677e0412d4732964e61faeafcc7f3a1690526
Soham Verma <sohamverma26@gmail.com>
2024-12-17 14:34:57 +1100

    API endpoints added

--- basestationproject/backendapi/views.py
+++ basestationproject/backendapi/views.py
@@ -1,6 +1,12 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.core.cache import cache
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+
+
 import json
 
 # Store mock devices in Redis
@@ -49,3 +55,55 @@ def connect_rover(request):
 def disconnect_rover(request):
     cache.set("rover_status", "disconnected", timeout=None)
     return JsonResponse({"status": "disconnected", "message": "Rover disconnected successfully!"})
+
+# Command-Related Views
+class RoverCommand(APIView):
+    def get(self, request):
+        pass # Functionality to be added later
+
+# Arm Control Views
+class ArmOverview(APIView):
+    def get(self, request):
+        pass # Functionality to be added later
+
+class ArmActivate(APIView):
+    def get(self, request):
+        return Response({"message": "Arm activation placeholder."}, status=status.HTTP_200_OK)
+
+class ArmDeactivate(APIView):
+    def get(self, request):
+        return Response({"message": "Arm deactivation placeholder."}, status=status.HTTP_200_OK)
+
+# Rover-control related views
+class RoverMove(APIView):
+    def get(self, request):
+        return Response({"message": "Rover move placeholder."}, status=status.HTTP_200_OK)
+
+class RoverStart(APIView):
+    def get(self, request):
+        return Response({"message": "Rover start placeholder."}, status=status.HTTP_200_OK)
+
+class RoverStop(APIView):
+    def get(self, request):
+        return Response({"message": "Rover stop placeholder."}, status=status.HTTP_200_OK)
+
+
+
+# class ArmActivate(APIView):
+#     def get(self, request):
+#         pass # Activate arm logic
+#
+# class ArmDeactivate(APIView):
+#     def get(self, request):
+#         pass  # Deactivate arm logic
+#
+#
+# # Rover Control Views
+# class RoverMove(APIView):
+#     def get(self, request):
+#         pass   # Rover move logic
+#
+#
+# class RoverStop(APIView):
+#     def get(self, request):
+#         pass  # Rover stop logic
